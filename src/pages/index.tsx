d87645fef6bf6789d2eefe0456630ddf67be1003
ポケモンサトシ <131662898+1F10230292@users.noreply.github.com>
2023-06-15 18:47:27 +0900

    a

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -6,22 +6,56 @@ const Home = () => {
 
   const [board, setBoard] = useState([
     [0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 1, 2, 0, 0, 0],
-    [0, 0, 0, 2, 1, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0],
+    [1, 1, 1, 1, 1, 1, 1, 0],
+    [1, 2, 2, 2, 2, 2, 1, 0],
+    [1, 2, 2, 2, 2, 2, 1, 0],
+    [1, 2, 2, 0, 2, 2, 1, 0],
+    [1, 2, 2, 2, 2, 2, 1, 0],
+    [1, 2, 2, 2, 2, 2, 1, 0],
+    [1, 1, 1, 1, 1, 1, 1, 0],
   ]);
+  const directions = [
+    [1, -1],
+    [0, -1],
+    [-1, -1],
+    [-1, 0],
+    [-1, 1],
+    [0, 1],
+    [1, 1],
+    [1, 0],
+  ];
   const onClick = (x: number, y: number) => {
     console.log(x, y);
     const newBoard: number[][] = JSON.parse(JSON.stringify(board));
-    if (board[y + 1] !== undefined && board[y + 1][x] === 3 - turnColor) {
-      newBoard[y][x] = turnColor;
+    let isPlace = false;
+    for (const direction of directions) {
+      let isPassAnotherColor = false;
+      for (let n = 1; n < 8; n += 1) {
+        if (
+          board[y + n * direction[0]] === undefined ||
+          board[y + n * direction[0]][x + n * direction[1]] === undefined
+        ) {
+          break;
+        } else if (board[y + n * direction[0]][x + n * direction[1]] === 0) {
+          break;
+        } else if (board[y + n * direction[0]][x + n * direction[1]] === 3 - turnColor) {
+          isPassAnotherColor = true;
+          continue;
+        } else if (
+          board[y + n * direction[0]][x + n * direction[1]] === turnColor &&
+          isPassAnotherColor
+        ) {
+          for (let m = 1; m <= n; m += 1) {
+            newBoard[y + (n - m) * direction[0]][x + (n - m) * direction[1]] = turnColor;
+          }
+          isPlace = true;
+          break;
+        }
+      }
+    }
+    if (isPlace) {
       setTurnColor(3 - turnColor);
     }
-
     setBoard(newBoard);
   };
   return (

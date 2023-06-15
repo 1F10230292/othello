ff177b7dbf9b9217c51179a543dbcb149603b849
1F10230292 <1F10230292@users.noreply.github.com>
2023-06-15 09:48:46 +0000

    deploy: d87645fef6bf6789d2eefe0456630ddf67be1003

---
+++ _next/static/BsTb6kg5BUuo5hwjPhli1/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/d98ecca65a293ad7.css","static/chunks/pages/index-3c328611ffc1a345.js"],"/_error":["static/chunks/pages/_error-54de1933a164a1ff.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
--- _next/static/ziQkD2PePUd_mdO4QV8fD/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/d98ecca65a293ad7.css","static/chunks/pages/index-cf791df3f69106b6.js"],"/_error":["static/chunks/pages/_error-54de1933a164a1ff.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
